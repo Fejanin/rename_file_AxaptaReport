ebcf14176c3f8404f5d7d229371b9df7815f540e
Shvec Sergei <fejanin@mail.ru>
2023-11-20 15:48:16 +0300

    Created a script for renaming files.

---
+++ main.py
@@ -0,0 +1,64 @@
+import PyPDF2
+import re
+from datetime import datetime
+import glob
+import os
+
+
+def create_new_name(old_file_name: str) -> list:
+    with open(old_file_name, 'rb') as pdfFileObj:
+        pdfReader = PyPDF2.PdfReader(pdfFileObj)
+        pageObj = pdfReader.pages[0]
+        text = pageObj.extract_text()
+    pattern1 = r'[a-zA-Zа-яА-ЯёЁ]{2}\d{6}\u00ad\d{2}'
+    number = re.findall(pattern1, text)[0].replace('\u00ad', '-')
+    pattern2 = r'\d{2}\.\d{2}\.\d{4}'
+    dates = re.findall(pattern2, text)
+    max_data = '.'.join(list(reversed(str(max([datetime.strptime(i, '%d.%m.%Y') for i in dates]))[:10].split('-'))))
+    new_file_name = f'{number} {max_data}.pdf'
+    if len(new_file_name) == 26:
+        return [old_file_name, new_file_name]
+    return None
+
+
+def find_pdf_files() -> list:
+    return glob.glob('AxaptaReport*.pdf')
+
+
+def check_unique(data: list) -> list:
+    all_files = {}
+    error = {}
+    for i in data:
+        if not i[1] in all_files:
+            all_files[i[1]] = i[0]
+        else:
+            td = error.get(i[1], [all_files[i[1]]])
+            td.append(i[0])
+            error[i[1]] = td
+    return error
+
+
+def rename_file(old_name: str, new_name: str) -> None:
+    os.rename(old_name, new_name)
+
+
+def create_error_report(error: dict) -> None:
+    with open('ERROR.txt', 'w') as f:
+        for key, value in error.items():
+            f.write(f'{key}: {value}')
+ 
+if __name__ == '__main__':
+    pdf_files = find_pdf_files()
+    old_new_files = [create_new_name(i) for i in pdf_files]
+    print(f'{old_new_files = }')
+    error = check_unique(old_new_files)
+    print(f'{error = }')
+    not_rename = [item for sublist in error.values() for item in sublist]
+    print(f'{not_rename = }')
+    for old, new in old_new_files:
+        if not old in not_rename:
+            rename_file(old, new)
+    if error:
+        create_error_report(error)
+
+
